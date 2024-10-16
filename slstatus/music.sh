c99c970e690500c4ddc3071bf2498f20efecd74c
Netanel Friedman <netanel.friedman6@gmail.com>
2024-10-16 14:25:44 +0300

    Remove the music.sh script

--- slstatus/music.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-status=$(playerctl status 2> /dev/null)
-if [[ $status != "Playing" ]]; then
-    echo -n "^c#666666^"
-fi
-
-echo -n "$(playerctl metadata artist 2> /dev/null) - $(playerctl metadata title 2> /dev/null)"
-echo -n "^d^"
