b63cb5540285980d0b7343070cbe3a863fc65e61
Netanel Friedman <netanel.friedman6@gmail.com>
2024-10-22 16:01:37 +0300

    Make the cpu component show spaced filled 2 digits

--- slstatus/components/cpu.c
+++ slstatus/components/cpu.c
@@ -43,7 +43,7 @@
 		if (sum == 0)
 			return NULL;
 
-		return bprintf("%d", (int)(100 *
+		return bprintf("%2d", (int)(100 *
 		               ((b[0] + b[1] + b[2] + b[5] + b[6]) -
 		                (a[0] + a[1] + a[2] + a[5] + a[6])) / sum));
 	}
