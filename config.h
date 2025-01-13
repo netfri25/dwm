ce88bf16cc6459c4d70813a4c7db65f4588e2fc7
Netanel Friedman <netanel.friedman6@gmail.com>
2025-01-13 14:45:29 +0200

    Remove firefox and osu! from the custom rules

--- config.h
+++ config.h
@@ -38,8 +38,6 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       1,           -1 },
-    { "osu!",     NULL,       NULL,       0,            1,           -1 },
 };
 
 /* layout(s) */
