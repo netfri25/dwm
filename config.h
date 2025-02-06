5a86851645b02d475a15f33780a9e154178f4bd5
Netanel Friedman <netanel.friedman6@gmail.com>
2025-02-06 15:41:07 +0200

    Change terminal from alacritty to kitty

--- config.h
+++ config.h
@@ -67,7 +67,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char* dmenucmd[] = { "rofi", "-show", "drun", "-theme", "gruvbox-dark-hard", NULL };
-static const char* termcmd[]  = { "alacritty", NULL };
+static const char* termcmd[]  = { "kitty", NULL };
 
 #define SAVE_SCREENSHOT "tee $HOME/Pictures/Screenshots/$(date +'Screenshot-from-%Y-%m-%d-%H-%M-%S').png | xclip -selection clipboard -t image/png"
 #define SCREENSHOT_CMD           "maim -u | "       SAVE_SCREENSHOT
