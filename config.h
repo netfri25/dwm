f305010e191ec3d6a86c75d2815c3cd7829bf7c5
Netanel Friedman <netanel.friedman6@gmail.com>
2025-05-19 19:31:31 +0300

    Don't force a theme on rofi

--- config.h
+++ config.h
@@ -69,7 +69,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char* dmenucmd[] = { "rofi", "-show", "drun", "-theme", "gruvbox-dark-hard", NULL };
+static const char* dmenucmd[] = { "rofi", "-show", "drun", NULL };
 static const char* termcmd[]  = { "alacritty", NULL };
 
 #define SAVE_SCREENSHOT "tee $HOME/Pictures/Screenshots/$(date +'Screenshot-from-%Y-%m-%d-%H-%M-%S').png | xclip -selection clipboard -t image/png"
